9fe39b4987c2d3c9da46e787a28f8d8037756bfd
BrunoBelarminoNog <brunobelarm.nog@gmail.com>
2021-01-26 12:06:28 -0300

    First commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+import db from '../db.json'
+
+const BackgroudImg = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`
+
+export default function Home() {
+  return (
+    <BackgroudImg>
+      ddsfsdfsdfsdffsdfds
+    </BackgroudImg>
+  )
+}
